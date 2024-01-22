4cd1cbdb69b5fabb54dd9f8ab189b9fce38bb678
Clinton Rocha <50300876+Clintonrocha98@users.noreply.github.com>
2024-01-21 21:15:00 -0300

    frist commit

---
+++ back/server.ts
@@ -0,0 +1,67 @@
+import express, { Request, Response } from "express";
+import { Pool } from "pg";
+
+const app = express();
+
+const pool = new Pool({
+  user: "user",
+  host: "postgres",
+  database: "postgres",
+  password: "password",
+  port: 5432,
+});
+
+app.use(express.json());
+
+async function createTable() {
+  try {
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS tasks (
+        ID SERIAL PRIMARY KEY,
+        task VARCHAR(255)
+      )
+    `;
+    await pool.query(createTableQuery);
+    console.log("Tabela 'tasks' criada com sucesso");
+  } catch (error) {
+    console.error("Erro ao criar a tabela:", error);
+  }
+}
+
+createTable();
+
+app.post("/tasks", async (req: Request, res: Response) => {
+  const { task } = req.body;
+  try {
+    const result = await pool.query(
+      "INSERT INTO tasks (task) VALUES ($1) RETURNING *",
+      [task]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query("SELECT * FROM tasks ORDER BY id ASC");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
+app.delete("/tasks/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  try {
+    await pool.query("DELETE FROM tasks WHERE id = $1", [id]);
+    res.status(200).send(`Task deleted with ID: ${id}`);
+  } catch (error) {
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server run in locahost: 3000!!!!");
+});
---
+++ front/src/App.tsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { ChangeEvent, useEffect, useState } from "react";
+import "./styles.css";
+
+interface Task {
+  id: number;
+  description: string;
+}
+
+const App = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [task, setTask] = useState<string>("");
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get<Task[]>("http://localhost:3000/tasks");
+        setTasks(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar tarefas:", error);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value);
+  };
+
+  const handleAddTask = async () => {
+    if (task.trim() !== "") {
+      try {
+        const response = await axios.post<Task>("sua-api/tasks", {
+          description: task,
+        });
+        setTasks([...tasks, response.data]);
+        setTask("");
+      } catch (error) {
+        console.error("Erro ao adicionar tarefa:", error);
+      }
+    }
+  };
+
+  const handleDeleteTask = async (taskId: number) => {
+    try {
+      await axios.delete(`sua-api/tasks/${taskId}`);
+      setTasks(tasks.filter((t) => t.id !== taskId));
+    } catch (error) {
+      console.error("Erro ao excluir tarefa:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Adicionar nova tarefa"
+          value={task}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleAddTask}>Adicionar</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.description}
+            <button onClick={() => handleDeleteTask(task.id)}>Apagar</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default App;
